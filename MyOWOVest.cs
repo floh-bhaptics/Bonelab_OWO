b9eb82a306ff33bdc1f5e40b2de4fa5a846a3345
niko20010 <nikolaj.dombrow@dombrow.de>
2023-08-21 08:50:56 +0200

    Update MyOWOVest.cs
    remove erroneous await and add sleep after explicit IP connect

--- MyOWOVest.cs
+++ MyOWOVest.cs
@@ -37,7 +37,8 @@ namespace MyOwoVest
             if(File.Exists(IPFile))
             {
                 string IP=File.ReadAllText(IPFile);
-                await OWO.Connect(IP);
+                OWO.Connect(IP);
+                Thread.Sleep(100);
             }
             else
             {
